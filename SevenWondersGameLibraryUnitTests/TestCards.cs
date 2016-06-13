12c7e9839bba46163455a98796abce95d67b87c2
Sean Henderson <seanxhenderson@hotmail.com>
2016-06-13 03:24:39 -0700

    Added a Unit Test project for the game library

---
+++ SevenWondersGameLibraryUnitTests/TestCards.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SevenWondersGameLibraryUnitTests
+{
+    [TestClass]
+    public class TestCards
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SevenWondersGameLibraryUnitTests/TestResources.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenWondersGameLibraryUnitTests
+{
+    [TestClass]
+    public class TestResources
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
